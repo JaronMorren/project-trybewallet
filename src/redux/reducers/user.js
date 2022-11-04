d480405cec0031538b2e1f226b1a43c80779501c
jaron morren <jaronmorren@gmail.com>
2022-11-04 15:13:57 -0300

    Added provider and userReducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIALSTATE = {
+  email: '', // string que armazena o email da pessoa usuária
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+  case 'VALID_USER':
+    return ({ email: action.email });
+  default:
+    return state;
+  }
+};
+
+export default user;
